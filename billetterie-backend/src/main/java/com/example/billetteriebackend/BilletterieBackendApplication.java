b53eced70b33a37efcaad818c3032ba754243652
mouad kaaib <elcosto02@gmail.com>
2025-05-01 17:31:42 +0100

    backend initialization

---
+++ billetterie-backend/src/main/java/com/example/billetteriebackend/BilletterieBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.billetteriebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilletterieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilletterieBackendApplication.class, args);
+	}
+
+}
