f19d1f8e0ac7cdf2895f3dedf546f672ec7b2f6b
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-05-21 13:33:42 +0100

    add EventDetailsOrganizer

---
+++ billetterie-frontend/src/app/features/organizer/components/event-details-organizer/event-details-organizer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'event-details-organizer',
+  standalone: false,
+  templateUrl: './event-details-organizer.component.html',
+  styleUrl: './event-details-organizer.component.css'
+})
+export class EventDetailsOrganizerComponent {
+
+}
