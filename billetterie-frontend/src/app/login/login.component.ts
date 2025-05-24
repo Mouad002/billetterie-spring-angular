f30434286814cab671498351c4ef3b95b10de31a
latifaja <#2662019Ma?>
2025-05-24 10:01:19 +0200

    Security part 4

--- billetterie-frontend/src/app/login/login.component.ts
+++ billetterie-frontend/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+
 @Component({
   selector: 'app-login',
   standalone: false,
@@ -28,7 +29,7 @@ formLogin! :FormGroup;
     this.authService.login(username,password).subscribe({
       next: (data) => {
 this.authService.loadProfile(data);   
-this.reter.navigateByUrl("/admin-panel");
+this.reter.navigateByUrl("/ticket-selection");
 },
       error: (err) => {
         console.log(err);
